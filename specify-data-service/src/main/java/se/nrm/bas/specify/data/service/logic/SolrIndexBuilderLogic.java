c4df09241537014257fd80c0586cb9538cdaf8c6
Ida Li <ida.li@nrm.se>
2020-02-21 14:12:18 +0100

    Add batch fetch

--- specify-data-service/src/main/java/se/nrm/bas/specify/data/service/logic/SolrIndexBuilderLogic.java
+++ specify-data-service/src/main/java/se/nrm/bas/specify/data/service/logic/SolrIndexBuilderLogic.java
@@ -1,12 +1,14 @@
 package se.nrm.bas.specify.data.service.logic;
 
 import java.io.Serializable;
-import java.util.List; 
+import java.util.Date;
+import java.util.List;   
 import javax.inject.Inject;
 import javax.json.JsonArray; 
-import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.Slf4j; 
 import se.nrm.bas.specify.data.service.logic.data.DataReader; 
 import se.nrm.bas.specify.data.service.logic.json.JsonConverter; 
+import se.nrm.bas.specify.data.service.logic.util.Util;
 import se.nrm.bas.specify.data.service.solr.SolrIndexBuilder;
 import se.nrm.dina.datamodel.EntityBean; 
 
@@ -24,17 +26,27 @@ public class SolrIndexBuilderLogic implements Serializable {
   @Inject
   private SolrIndexBuilder indexBuilder;
   
-  
+  private final int batch = 2000; 
+  private int end;
   
   public SolrIndexBuilderLogic() {
     
   }
    
-  public void run(String institution, int collectionCode, String fromDate, String toDate) {
-    List<EntityBean> list = reader.findAll(institution, collectionCode, fromDate, toDate);
-  
-    JsonArray json = converter.convert(list, institution, collectionCode); 
- 
-    indexBuilder.postToSolr("nrm_index", json.toString()); 
+  public void run(String institution, int collectionCode, String strFromDate, String strToDate) {
+    
+    Date fromDate = Util.getInstance().stringToDate(strFromDate);
+    Date toDate = Util.getInstance().stringToDate(strToDate);
+    
+    boolean isNrm = Util.getInstance().isNrm(institution);  
+    List<Integer> ids = reader.getCollectionIds(collectionCode, fromDate, toDate, isNrm); 
+    
+    int total = ids.size();
+    for (int i = 0; i < total; i += batch) {  
+      end = i + batch <= total ? i + batch : total;
+      List<EntityBean> list = reader.fetchData(institution, collectionCode, fromDate, toDate, ids.subList(i, end), isNrm); 
+      JsonArray json = converter.convert(list, institution, collectionCode);  
+      indexBuilder.postToSolr(Util.getInstance().getIndexCore(institution), json.toString());  
+    }     
   }
 }
