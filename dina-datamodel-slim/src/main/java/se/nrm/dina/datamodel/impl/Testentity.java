3acc5194eb0f5ddac67ca932ce435c12134fe962
Ida Li <ida.li@nrm.se>
2020-03-23 20:10:52 +0100

    Add extra fields into TestEntity for testing

--- dina-datamodel-slim/src/main/java/se/nrm/dina/datamodel/impl/Testentity.java
+++ dina-datamodel-slim/src/main/java/se/nrm/dina/datamodel/impl/Testentity.java
@@ -1,93 +1,126 @@
 package se.nrm.dina.datamodel.impl;
 
-import java.math.BigDecimal; 
-import java.util.List; 
-import javax.persistence.Id; 
-import javax.xml.bind.annotation.XmlTransient;
-//import org.codehaus.jackson.annotate.JsonIgnoreProperties;
-import se.nrm.dina.datamodel.BaseEntity; 
+import java.math.BigDecimal;  
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlTransient; 
+import se.nrm.dina.datamodel.BaseEntity;
+import se.nrm.dina.datamodel.EntityBean;
 
 /**
  *
  * @author idali
- */
-//@JsonIgnoreProperties(ignoreUnknown = true)
+ */ 
 public class Testentity extends BaseEntity {
-    
-    @Id
-    private int id;
-    
-//    private String identityString;
-//    private int entityId;
-    
-    private Agent createdByAgentID;
-    private List<String> testList;
-    private BigDecimal bgDecimal;
-    private short s;
-    private String string;
-    
-    
-    public Testentity() {
-        
-    }
-    
-    public Testentity(int id) {
-        this.id = id;
-    }
+
+  @Id
+  private int id;
  
-    @XmlTransient 
-    @Override 
-    public String getIdentityString() {
-        return String.valueOf(id);
-    }
+  private Agent createdByAgentID;
+  private List<String> testList;
+  private BigDecimal bgDecimal;
+  private short s;
+  private String string;
+  private boolean isTrue;
+  private java.util.Date date1;
+  private java.sql.Date date2;
+  private List<EntityBean> beans;
 
-    @XmlTransient 
-    @Override
-    public int getEntityId() {
-        return id;
-    }
+  public Testentity() {
 
-    public int getId() {
-        return id;
-    } 
+  }
 
-    public Agent getCreatedByAgentID() {
-        return createdByAgentID;
-    }
+  public Testentity(int id) {
+    this.id = id;
+  }
 
-    public void setCreatedByAgentID(Agent createdByAgentID) {
-        this.createdByAgentID = createdByAgentID;
-    } 
+  @XmlTransient
+  @Override
+  public String getIdentityString() {
+    return String.valueOf(id);
+  }
 
-    public BigDecimal getBgDecimal() {
-        return bgDecimal;
-    }
+  @XmlTransient
+  @Override
+  public int getEntityId() {
+    return id;
+  }
 
-    public void setBgDecimal(BigDecimal bgDecimal) {
-        this.bgDecimal = bgDecimal;
-    }
+  public int getId() {
+    return id;
+  }
 
-    public short getS() {
-        return s;
-    }
+  public Agent getCreatedByAgentID() {
+    return createdByAgentID;
+  }
 
-    public void setS(short s) {
-        this.s = s;
-    }
+  public void setCreatedByAgentID(Agent createdByAgentID) {
+    this.createdByAgentID = createdByAgentID;
+  }
 
-    public String getString() {
-        return string;
-    }
+  public BigDecimal getBgDecimal() {
+    return bgDecimal;
+  }
 
-    public void setString(String string) {
-        this.string = string;
-    }
-     
-    public List<String> getTestList() {
-        return testList;
-    }
+  public void setBgDecimal(BigDecimal bgDecimal) {
+    this.bgDecimal = bgDecimal;
+  }
 
-    public void setTestList(List<String> testList) {
-        this.testList = testList;
-    }  
+  public short getS() {
+    return s;
+  }
+
+  public void setS(short s) {
+    this.s = s;
+  }
+
+  public String getString() {
+    return string;
+  }
+
+  public void setString(String string) {
+    this.string = string;
+  }
+
+  public List<String> getTestList() {
+    return testList;
+  }
+
+  public void setTestList(List<String> testList) {
+    this.testList = testList;
+  }
+
+  public boolean isIsTrue() {
+    return isTrue;
+  }
+
+  public void setIsTrue(boolean isTrue) {
+    this.isTrue = isTrue;
+  }
+
+  public Date getDate1() {
+    return date1;
+  }
+
+  public void setDate1(Date date1) {
+    this.date1 = date1;
+  }
+
+  public java.sql.Date getDate2() {
+    return date2;
+  }
+
+  public void setDate2(java.sql.Date date2) {
+    this.date2 = date2;
+  }
+   
+  public List<EntityBean> getBeans() {
+    return beans;
+  }
+
+  public void setBeans(List<EntityBean> beans) {
+    this.beans = beans;
+  }
+
 }
