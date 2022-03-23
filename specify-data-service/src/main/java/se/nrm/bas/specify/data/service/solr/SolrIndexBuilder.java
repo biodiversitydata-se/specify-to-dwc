4e68da8ed1d5ccb3bf374d026fa938976e0bc26f
Ida Li <ida.li@nrm.se>
2022-03-23 08:59:39 +0100

    Update SolrIndexBuilder.java

--- specify-data-service/src/main/java/se/nrm/bas/specify/data/service/solr/SolrIndexBuilder.java
+++ specify-data-service/src/main/java/se/nrm/bas/specify/data/service/solr/SolrIndexBuilder.java
@@ -1,6 +1,6 @@
 package se.nrm.bas.specify.data.service.solr;
 
-import java.io.IOException;   
+import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;  
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -19,11 +19,7 @@ public class SolrIndexBuilder {
   private final String utf8 = "UTF-8";
   private final String applicationJson = "application/json";
   private HttpResponse response; 
-  
-  private HttpClient client;
-  private HttpPost post;
-  private StringEntity entity; 
-  
+    
   private int status;
  
   public SolrIndexBuilder() {
@@ -33,16 +29,17 @@ public class SolrIndexBuilder {
   public int postToSolr(String solrUpdateUrl, String jsonString) { 
     log.info("postToSolr");
     
-    client = HttpClientBuilder.create().build();
+    HttpClient client = HttpClientBuilder.create().build();
   
-    post = new HttpPost(solrUpdateUrl);
+    HttpPost post = new HttpPost(solrUpdateUrl);
  
-    entity = new StringEntity(jsonString, utf8);
+    StringEntity entity = new StringEntity(jsonString, utf8);
     entity.setContentType(applicationJson);
     post.setEntity(entity); 
     try {
       response = client.execute(post);  
       status = response.getStatusLine().getStatusCode(); 
+      log.info("status : {}", status);
       return status;
     } catch (IOException ex) {
       log.error(ex.getMessage());
